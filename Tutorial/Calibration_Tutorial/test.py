dfa14d4d7f93632e33b8de2a584c057e7ae0e45f
juntae02 <juntaepark02@gmail.com>
2025-06-10 15:32:33 +0900

    yolo model

--- Tutorial/Calibration_Tutorial/test.py
+++ Tutorial/Calibration_Tutorial/test.py
@@ -72,7 +72,8 @@ class TestNode(Node):
 
     def pick_and_drop(self, x, y, z):
         current_pos = get_current_posx()[0]
-        pick_pos = posx([x, y, z, current_pos[3], current_pos[4], current_pos[5]])
+        self.z_ = z-20
+        pick_pos = posx([x, y, self.z_, current_pos[3], current_pos[4], current_pos[5]])
         # TODO: Write pick and drop function
         movel(pick_pos, vel=VELOCITY, acc=ACC)
         self.gripper.close_gripper()
---
+++ Tutorial/Ultralytics/data/README.dataset.txt
@@ -0,0 +1,6 @@
+# myscr > 2024-04-29 7:21pm
+https://universe.roboflow.com/yolov5-dmrkh/myscr
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
