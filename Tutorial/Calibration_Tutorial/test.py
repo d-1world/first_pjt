14d1f9b2c117d9c3fdb280b7091f3865d9201ec9
KIMYOHAN <kssg121340@gmail.com>
2025-06-10 10:40:26 +0900

    add

--- Tutorial/Calibration_Tutorial/test.py
+++ Tutorial/Calibration_Tutorial/test.py
@@ -72,7 +72,8 @@ class TestNode(Node):
 
     def pick_and_drop(self, x, y, z):
         current_pos = get_current_posx()[0]
-        pick_pos = posx([x, y, z, current_pos[3], current_pos[4], current_pos[5]])
+        safe_z=z-20
+        pick_pos = posx([x, y, safe_z, current_pos[3], current_pos[4], current_pos[5]])
         # TODO: Write pick and drop function
         movel(pick_pos, vel=VELOCITY, acc=ACC)
         self.gripper.close_gripper()
